6a681d90151f6068591fb670d72899eb2a93e281
ibrahim-hassann <ih1121369@gmail.com>
2025-04-22 20:41:25 +0200

    mmm

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const HomePage = () => {
+  return (
+    <><div>
+        <h1>Home Page</h1>
+    
+    </div>
+    <div>
+        <h2>Home Page</h2>
+        <h1>ibrahim</h1>
+        </div></>
+  )
+}
