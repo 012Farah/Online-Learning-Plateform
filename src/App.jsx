7436fca4308f410ebe71bff96baad29d937b9b94
Farah Mohamed <fara7.zabadi@gmail.com>
2025-04-22 20:57:24 +0200

    mm

--- src/App.jsx
+++ src/App.jsx
@@ -17,17 +17,23 @@ export default function App() {
       <BrowserRouter>
       <Navbar />
       <Routes>
+<<<<<<< Updated upstream
 
         <Route path="/" element={<HomePage />} />
         <Route path="/index" element={<HomePage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/homepage" element={<HomePage />} />
+=======
+        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/home-page" element={<HomePage />} />
+>>>>>>> Stashed changes
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/contact-us" element={<ContactUs />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/forget-password" element={<ForgotPassword />} />
-        
+
       </Routes>
       </BrowserRouter>
     </div>
