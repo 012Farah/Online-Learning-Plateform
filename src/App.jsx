441e40c0162d179e08bced263f2f79a3b680c9f4
ibrahim-hassann <ih1121369@gmail.com>
2025-04-22 20:56:02 +0200

    mm

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 
+
 import Navbar from "./components/Navbar";
 import AboutUs from "./Pages/AboutUs";
 import ContactUs from "./Pages/ContactUs";
 import SignIn from "./Pages/SignIn";
 import SignUp from "./Pages/SignUp";
 import ForgotPassword from "./Pages/ForgotPassword.jsx";
+import { HomePage } from "./Pages/HomePage.jsx";
 
 export default function App() {
   return (
@@ -15,6 +17,11 @@ export default function App() {
       <BrowserRouter>
       <Navbar />
       <Routes>
+
+        <Route path="/" element={<HomePage />} />
+        <Route path="/index" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/homepage" element={<HomePage />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/contact-us" element={<ContactUs />} />
         <Route path="/sign-in" element={<SignIn />} />
