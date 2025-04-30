55fd6dd9d4b4943889bd040c5127cab05f2efdde
mohamedmodar <mohameddowidar555@gmail.com>
2025-04-30 17:32:10 +0300

    add 404 page not found && function in app to make some page with navbar and some page without

--- src/App.jsx
+++ src/App.jsx
@@ -15,35 +15,48 @@ import Settings from "./Admin/setting/setting.jsx";
 import Layout from "./Admin/Layout/Layout.jsx";
 import  { Blog } from "./Pages/Blog.jsx";
 import Singlepage from "./Pages/Singlepage.jsx";
+import NotFound from './Pages/NotFound.jsx';
+
+
+
 
 export default function App() {
-  return (
-    <div className="app">
-      <BrowserRouter>
-      <Navbar />
-      <Routes>
 
-        <Route path="/" element={<HomePage />} />
-        <Route path="/index" element={<HomePage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/homepage" element={<HomePage />} />
-        <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/contact-us" element={<ContactUs />} />
+ 
+  
+  return (
+    
+    <div className="app">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RouteWithNavbar element={HomePage} />} />
+        <Route path="/index" element={<RouteWithNavbar element={HomePage} />} />
+        <Route path="/home" element={<RouteWithNavbar element={HomePage} />} />
+        <Route path="/homepage" element={<RouteWithNavbar element={HomePage} />} />
+        <Route path="/about-us" element={<RouteWithNavbar element={AboutUs} />} />
+        <Route path="/contact-us" element={<RouteWithNavbar element={ContactUs} />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/forget-password" element={<ForgotPassword />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/:id" element={<Singlepage />} />
-
-       
-
-        <Route path="/admin" element={<Layout />}>
-        <Route index element={<Homedash />} />
-        <Route path="setting" element={<Settings />} />
+        <Route path="/blog" element={<RouteWithNavbar element={Blog} />} />
+        <Route path="/blog/:id" element={<RouteWithNavbar element={Singlepage} />} />
+        <Route path="/404" element={<NotFound />} /> 
+            <Route path="*" element={<NotFound />} /> 
+        <Route path="/admin" element={<Layout />}> 
+          <Route index element={<Homedash />} />
+          <Route path="setting" element={<Settings />} />
         </Route>
-        
       </Routes>
-      </BrowserRouter>
-    </div>
+    </BrowserRouter>
+  </div>
+  );
+}
+// function to add the navbar to specifc pages
+function RouteWithNavbar({ element: Component, ...rest }) {
+  return (
+    <>
+      <Navbar />
+      <Component />
+    </>
   );
 }
